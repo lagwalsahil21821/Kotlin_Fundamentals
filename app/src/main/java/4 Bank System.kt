e17e3381c61287fc676dff7170c228ab8eff3100
lagwalsahil21821 <lagwalsahil7978@gmail.com>
2023-02-08 22:50:52 +0530

    completed

--- app/src/main/java/4 Bank System.kt
+++ app/src/main/java/4 Bank System.kt
@@ -1,9 +1,12 @@
-/*class Bank(val accNo:Int, val balance:Double){
-
-}*/
 var balance = 50000 // initial balance
-
+var accountType = ""
+var money: Int = 0
 fun withdraw(amount: Int):Int{
+    balance -= amount
+    println("You successfully withdrew $amount dollars. The current balance is $balance dollars.")
+    return amount
+}
+fun debitWithdraw(amount: Int):Int{
     if(amount == 0){
         println("Can't withdraw. No money on this account!!!")
         return balance
@@ -41,10 +44,35 @@ fun creditDeposit(amount: Int):Int{
     }
 }
 
+fun transfer(mode: String){
+    val transferAmount: Int
+
+    when(mode){
+        "withdraw" -> {
+            if(accountType == "Debit"){
+                transferAmount = debitWithdraw(money)
+            }else{
+                transferAmount = withdraw(money)
+            }
+            println("The amount you withdrew is $transferAmount dollars.")
+        }
+        "deposit" -> {
+            if(accountType == "Credit"){
+                transferAmount = creditDeposit(money)
+            }else{
+                transferAmount = deposit(money)
+            }
+            println("The amount you deposit is $transferAmount dollars")
+        }
+        else -> return
+
+    }
+}
+
 fun main(){
     println("Welcome to your Banking System\nWhat type of account would you like to create")
     println("1. Debit Account\n2. Credit Account\n3. Checking\nChoose an option (1, 2 or 3)")
-    var accountType = ""
+
     var userChoice = 0
 
     while(accountType == "") {
@@ -68,22 +96,35 @@ fun main(){
 
     println("The current account balance is $balance dollars")
     println("Enter amount to be transferred: ")
-    val money = readln().toInt()
+    money = readln().toInt()
     println("The amount you transferred is $money dollars")
 
-    var output= 0
-    output = withdraw(money)
-    println("The amount you withdrew is $output dollars.")
+// part 3
+    println("*****---------*****\n")
 
+    var isSystemOpen = true
+    var option = 0
 
-    output = deposit(money)
-    println("The amount you deposited is $output dollars.")
+    while(isSystemOpen == true){
+        println("\nWhat would you like to do? ")
+        println("1. Check Account Balance")
+        println("2. Withdraw Money")
+        println("3. Deposit Money")
+        println("4. Close the System")
+        println("Which option do you choose? (1, 2, 3 or 4)")
+        option = readln().toInt()
 
-
-// deposit operation for credit account
-    output = creditDeposit(money)
-    println("The amount you deposited is ${output} dollars.")
-
+        when(option){
+            1 -> println("The current balance is $balance dollars.")
+            2 -> transfer("withdraw")
+            3 -> transfer("deposit")
+            4 -> {
+                isSystemOpen = false
+                println("The system is closed")
+            }
+            else -> continue
+        }
+    }
 
 
 }
